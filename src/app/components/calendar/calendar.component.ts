da7b56013f162e81c8529300a174cb485f8df89f
Daniil Kulikov <dkulikov@bivgroup.ru>
2023-10-16 21:56:38 +0300

    changes

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { addMonths, subMonths, getDaysInMonth, startOfMonth, endOfMonth, addDays, isWeekend, getDay } from 'date-fns';
-import { weekdays } from 'moment';
 
 @Component({
   selector: 'app-calendar',
