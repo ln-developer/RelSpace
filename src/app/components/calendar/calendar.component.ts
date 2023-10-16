6c8f23d14757c581f6a2459cff3f71517040cea6
Daniil Kulikov <dkulikov@bivgroup.ru>
2023-10-16 23:28:00 +0300

    changes

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -7,12 +7,10 @@ import { addMonths, subMonths, getDaysInMonth, startOfMonth, endOfMonth, addDays
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-  currentDate: Date = new Date();
   selectedMonth: Date = new Date();
   calendarDays: Date[][] = [];
 
   constructor() {
-    this.selectedMonth = new Date();
   }
 
   ngOnInit(): void {
@@ -26,48 +24,26 @@ export class CalendarComponent implements OnInit {
 
     let currentWeek: Date[] = [];
 
-    const startDayOfWeek = getDay(firstDay);
-    const daysInPreviousMonth = startDayOfWeek - 1;
+    let currentDate = firstDay;
 
-    // Добавляем дни предыдущего месяца, если начало недели не в понедельник
-    for (let i = daysInPreviousMonth; i > 0; i--) {
-      const date = subMonths(firstDay, 1);
-      date.setDate(getDaysInMonth(date) - i + 1);
-      currentWeek.push(date);
+    // Находим первый будний день месяца
+    while (isWeekend(currentDate)) {
+      currentDate = addDays(currentDate, 1);
     }
 
-    for (let date = firstDay; date <= lastDay; date = addDays(date, 1)) {
-      if (!isWeekend(date)) {
-        currentWeek.push(date);
-        if (currentWeek.length === 5) {
-          this.calendarDays.push([...currentWeek]);
-          currentWeek = [];
+    for (let i = 0; i < 5; i++) { // Создаем 5 недель
+      for (let j = 0; j < 5; j++) { // Добавляем будние дни в неделю
+        if (currentDate <= lastDay) {
+          currentWeek.push(currentDate);
         }
+        currentDate = addDays(currentDate, 1);
       }
-    }
-
-    // Добавляем дни следующего месяца только, если текущий месяц не декабрь
-    if (this.selectedMonth.getMonth() < 11) {
-      const endDayOfWeek = getDay(lastDay);
-      const daysInNextMonth = 5 - endDayOfWeek;
-
-      if (daysInNextMonth > 0) {
-        for (let i = 1; i <= daysInNextMonth; i++) {
-          const date = addMonths(lastDay, 1);
-          date.setDate(i);
-          currentWeek.push(date);
-        }
-      }
-    }
-
-    if (currentWeek.length > 0) {
       this.calendarDays.push([...currentWeek]);
+      currentWeek = [];
     }
   }
-
   changeMonth(increment: number) {
     this.selectedMonth = increment > 0 ? addMonths(this.selectedMonth, 1) : subMonths(this.selectedMonth, 1);
     this.updateCalendar();
   }
-
 }
