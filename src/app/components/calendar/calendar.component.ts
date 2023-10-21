d57e33da2ec34446e966dbb96a25f63520b86645
zonder12120 <zonder12120@yandex.ru>
2023-10-21 23:00:37 +0300

    Some fixes, removed the use of objects, implemented checking the days of the month inside HTML, added hovered states

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,94 +1,91 @@
 import {Component, OnInit} from '@angular/core';
-import {addDays, addMonths, endOfMonth, getDate, getDay, isWeekend, startOfMonth, subMonths} from 'date-fns';
+import {
+  addDays,
+  addMonths,
+  endOfMonth,
+  getDate,
+  getDay, getMonth,
+  isWeekend,
+  startOfMonth,
+  subMonths
+} from 'date-fns';
+
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css'],
 })
+
 export class CalendarComponent implements OnInit {
+
   selectedMonth: Date = new Date();
-  selectedRowIndex: number = 0;
-  calendarWeeks: { day: Date; isCurrentMonth: boolean }[][] = [];
+  selectedRowIndex: number | null = null;
+  calendarWeeks: Date[][] = [];
   firstDaySelMonth: Date = startOfMonth(this.selectedMonth);
   lastDaySelMonth: Date = endOfMonth(this.selectedMonth);
+  hoveredRowIndex: number | null = null;
 
-  constructor() {}
-
+  constructor() {
+  }
 
   ngOnInit(): void {
     this.updateCalendar();
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.findCurrentWeek();
-
-    document.querySelectorAll('.calendar-week').forEach((row, index) => {
-      const isSelected = index === this.selectedRowIndex;
-      this.setRowStyles(row, isSelected);
-    });
-    this.updateMouseOverHandlers();
   }
 
   ngAfterViewChecked() {
-    document.querySelectorAll('.calendar-week').forEach((row, index) => {
-      const isSelected = index === this.selectedRowIndex;
-      this.setRowStyles(row, isSelected);
-    });
-    this.updateMouseOverHandlers();
+  }
+
+  isOtherMonth(day: Date){
+    return !(getMonth(day) === getMonth(this.selectedMonth));
   }
 
   findCurrentWeek() {
     let dayOfMonth = getDate(this.selectedMonth);
     let foundRowIndex = -1;
 
-    document.querySelectorAll('.calendar-week').forEach((row, index) => {
-      row.querySelectorAll('td').forEach((cell) => {
-        const cellContent = cell.textContent?.trim();
-        if (cellContent) {
-          const cellNumber = parseInt(cellContent, 10);
+    for (let cellNum = 0; cellNum < 2; cellNum++) {
+      document.querySelectorAll('.calendar-week').forEach((row, index) => {
+        row.querySelectorAll('td').forEach((cell) => {
+          const cellContent = cell.textContent?.trim();
+          if (cellContent) {
+            const cellDay = parseInt(cellContent, 10);
 
-          if (!isNaN(cellNumber) && cellNumber === dayOfMonth) {
-            foundRowIndex = index;
-            return;
+            if (!isNaN(cellDay) && cellDay === dayOfMonth) {
+              foundRowIndex = index;
+              return;
+            }
           }
-        }
+        });
       });
-    });
 
-    if (foundRowIndex >= 0) {
-      this.selectedRowIndex = foundRowIndex;
-    } else {
-      dayOfMonth--;
+      if (foundRowIndex >= 0) {
+        this.selectedRowIndex = foundRowIndex;
+        break;
+      } else {
+        dayOfMonth--;
+      }
     }
     console.log(this.selectedRowIndex);
+  }
+
+  handleRowHover(i: number | null) {
+    this.hoveredRowIndex = i;
   }
 
   handleRowClick(index: number) {
     this.selectedRowIndex = index;
     console.log(index);
-    document.querySelectorAll('.calendar-week').forEach((row, rowIndex) => {
-      row.querySelectorAll('td').forEach(cell => {
-        if (rowIndex === index) {
-          cell.style.backgroundColor = '#FF6C6C';
-          cell.style.color = 'white';
-        } else {
-          cell.style.backgroundColor = 'transparent';
-
-          if (cell.classList.contains('other-month')) {
-            cell.style.color = '#b4b4b4';
-          } else {
-            cell.style.color = 'black';
-          }
-        }
-      });
-    });
   }
 
   updateCalendar() {
     const prevMonthArr = this.generateMonthsArr(subMonths(this.selectedMonth, 1));
     const nextMonthArr = this.generateMonthsArr(addMonths(this.selectedMonth, 1));
-    const calendarDays = [];
+    let calendarDays: Date[] = [];
 
     for (let date = this.firstDaySelMonth; date <= this.lastDaySelMonth; date = addDays(date, 1)) {
 
@@ -102,7 +99,7 @@ export class CalendarComponent implements OnInit {
       }
 
       if (!isWeekend(date)) {
-        calendarDays.push({day: date, isCurrentMonth: true });
+        calendarDays.push(date);
       }
 
       if (date.setHours(0, 0, 0, 0) === this.lastDaySelMonth.setHours(0, 0, 0, 0)) {
@@ -118,9 +115,9 @@ export class CalendarComponent implements OnInit {
 
   }
 
-  splitArrayIntoWeeks(array: { day: Date; isCurrentMonth: boolean }[]): { day: Date; isCurrentMonth: boolean }[][] {
-    const weeks: { day: Date; isCurrentMonth: boolean }[][] = [];
-    let week: { day: Date; isCurrentMonth: boolean }[] = [];
+  splitArrayIntoWeeks(array: Date[]): Date[][] {
+    const weeks: Date[][] = [];
+    let week: Date[] = [];
 
     for (const dateObj of array) {
       week.push(dateObj);
@@ -134,85 +131,37 @@ export class CalendarComponent implements OnInit {
     return weeks;
   }
 
+  generateMonthsArr(month: Date) {
+
+    const firstDay = startOfMonth(month);
+    const lastDay = endOfMonth(month);
+    const monthDays: Date[] = [];
+
+    if (month < this.selectedMonth) {
+      for (let date: Date = firstDay; date <= lastDay; date = addDays(date, 1)) {
+        monthDays.unshift(date);
+      }
+    } else {
+      for (let date: Date = firstDay; date <= lastDay; date = addDays(date, 1)) {
+        monthDays.push(date);
+      }
+    }
+    return monthDays;
+  }
+
   changeMonth(increment: number) {
     this.selectedMonth = increment > 0 ? addMonths(this.selectedMonth, 1) : subMonths(this.selectedMonth, 1);
     this.firstDaySelMonth = startOfMonth(this.selectedMonth);
     this.lastDaySelMonth = endOfMonth(this.selectedMonth);
     this.updateCalendar();
-    this.selectedRowIndex = -1;
-  }
-
-  setRowStyles(row: Element, isSelected: boolean): void {
-    const color = isSelected ? '#FF6C6C' : 'transparent';
-    row.querySelectorAll('td').forEach(cell => {
-      cell.style.backgroundColor = color;
-
-      if (cell.classList.contains('other-month')) {
-        cell.style.color = isSelected ? 'white' : '#b4b4b4';
-      } else {
-        cell.style.color = isSelected ? 'white' : 'black';
-      }
-    });
-  }
-
-  updateMouseOverHandlers() {
-    document.querySelectorAll('.calendar-week').forEach(row => {
-      row.removeEventListener('mouseover', this.handleMouseOver as EventListener);
-      row.removeEventListener('mouseout', this.handleMouseOut as EventListener);
-      row.addEventListener('mouseover', this.handleMouseOver as EventListener);
-      row.addEventListener('mouseout', this.handleMouseOut as EventListener);
-    });
-  }
-
-  handleMouseOver = (event: MouseEvent) => {
-    const index = Array.from(document.querySelectorAll('.calendar-week')).indexOf(event.currentTarget as Element);
-
-    if (index !== this.selectedRowIndex) {
-      (event.currentTarget as Element).querySelectorAll('td').forEach(cell => {
-        cell.style.backgroundColor = '#f3f3f3';
-
-        if (cell.classList.contains('other-month')) {
-          cell.style.color = 'gray';
-        }
-      });
-    }
-  }
-
-  handleMouseOut = (event: MouseEvent) => {
-    const index = Array.from(document.querySelectorAll('.calendar-week')).indexOf(event.currentTarget as Element);
-
-    if (index !== this.selectedRowIndex) {
-      (event.currentTarget as Element).querySelectorAll('td').forEach(cell => {
-        cell.style.backgroundColor = 'transparent';
-
-        if (cell.classList.contains('other-month')) {
-          cell.style.color = '#b4b4b4';
-        }
-      });
-    }
+    this.selectedRowIndex = null;
   }
 
   formatMonthName(date: Date): string {
-    const month = new Intl.DateTimeFormat('ru', { month: 'long' }).format(date);
+    const month = new Intl.DateTimeFormat('ru', {month: 'long'}).format(date);
     const year = date.getFullYear();
     return `${month.charAt(0).toUpperCase() + month.slice(1)} ${year}`;
   }
 
-  generateMonthsArr(month: Date) {
 
-    const firstDay = startOfMonth(month);
-    const lastDay = endOfMonth(month);
-    const array: { day: Date; isCurrentMonth: boolean }[] = [];
-
-    if (month < this.selectedMonth) {
-      for (let date = firstDay; date <= lastDay; date = addDays(date, 1)) {
-        array.unshift({day: date, isCurrentMonth: false });
-      }
-    } else {for (let date = firstDay; date <= lastDay; date = addDays(date, 1)) {
-              array.push({day: date, isCurrentMonth: false });
-        }
-    }
-
-    return array;
-  }
 }
